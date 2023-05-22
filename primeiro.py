84b270f67514406132ad5ca85d25f1072bb53258
biacbarros <bia_cbarros@yahoo.com.br>
2023-05-21 21:45:49 -0300

    Add

---
+++ primeiro.py
@@ -0,0 +1,13 @@
+#Primeira aula
+#Print: uma função para imprimir alguma coisa na tela 
+print ('Hello World!')
+print('Seja bem-vinda ao curso de Python do Coding Tank!')
+
+input('Qual a linguagem de programação que você está estudando?')
+
+
+"""
+Para realizar um comentário de um bloco de código utilizamos 6 aspas duplas
+"""
+
+#Fim primeira aula
