1a904ce9cf673536cbacc5d50c974734a9cc1bd1
biacbarros <bia_cbarros@yahoo.com.br>
2023-05-21 22:24:07 -0300

    add

---
+++ conversao_de_tipos.py
@@ -0,0 +1,20 @@
+# Conversão de tipo
+
+idade = '26'
+print(idade, type(idade))
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))
+
+#int()
+#str()
+#float()
+#bool()
+
+altura = input('Informe sua altura ')
+print(altura, type(altura))
+
+altura2 = float(input('Informe sua altura '))
+print(altura2, type(altura2))
+
